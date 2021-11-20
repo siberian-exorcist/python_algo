8a63ce389026b14127fce7b2c97c27f9823a05a2
Yaroslav <71167669+siberian-exorcist@users.noreply.github.com>
2021-11-20 10:34:45 +0300

    les_7_task_3.py

---
+++ les_7_task_3.py
@@ -0,0 +1,33 @@
+"""
+Массив размером 2m + 1, где m – натуральное число, заполнен случайным образом. Найдите в массиве медиану.
+Медианой называется элемент ряда, делящий его на две равные части: в одной находятся элементы,
+которые не меньше медианы, в другой – не больше медианы. Задачу можно решить без сортировки исходного массива.
+Но если это слишком сложно, то используйте метод сортировки, который не рассматривался на уроках
+"""
+
+from random import uniform as rand_f
+
+m = int(input('Введите размер m массива 2m+1. m = '))
+start_ = -1000
+end_ = 1000
+arr_ = [round(rand_f(start_, end_), 2) for i in range(2*m+1)]
+
+print(f'Исходный массив:\n {arr_}')
+
+
+def mediana_1(arr):
+    for i in range(len(arr)):
+        cnt_less = 0
+        cnt_more = 0
+        for j in range(len(arr)):
+            if arr[i] > arr[j]:
+                cnt_less += 1
+            elif arr[i] == arr[j]:
+                continue
+            else:
+                cnt_more += 1
+        if cnt_less == cnt_more:
+            return arr[i]
+
+
+print(f'{mediana_1(arr_)}')
